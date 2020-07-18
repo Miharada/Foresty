9c83d4bedba02b2b41ec3b3c34e55aef47971f47
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2020-07-18 22:27:28 +0700

    Add files via upload

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -10,29 +10,52 @@ def get_ips_for_host(host):
             ips=[]
         return ips
 
+def writeout(texts):
+    f = open(args.o, "a+")
+    f.write(texts+"\n")
+    f.close()
+
 parser = argparse.ArgumentParser()
 parser.add_argument('url', type=str)
 parser.add_argument('-u', help='Display URL source code', action='store_true')
 parser.add_argument('-i', help='Display URL IP Adress', action='store_true')
 parser.add_argument('--header', help='Display URL Header',action='store_true')
+parser.add_argument('-o', help='Store output to txt file',type=str)
 
 args = parser.parse_args()
 
+response = ""
+
 print(args.url)
+
+if(args.o):
+     f = open(args.o, "w+")
 if(args.u):
-    r = requests.get(args.url)
-    page_source = r.text
+    response = requests.get(args.url)
+    page_source = response.text
     print("\nURL:", args.url)
     print(page_source)
+    if(args.o):
+        writeout(page_source)
+
 if(args.i):
     url = args.url.replace('http://','')
     url = args.url.replace('https://','')
     ips = get_ips_for_host(url)
     print(repr(ips))
+    if(args.o):
+        writeout(repr(ips))
 if(args.header):
-    response = requests.get(args.url)
+    if(not(args.u)):
+        response = requests.get(args.url)
     print(response)
     for i in response.headers:
         print(i,":",response.headers[i])
+        if(args.o):
+            writeout(texts = i+":"+response.headers[i])
+    
+    
+
+
 
 
