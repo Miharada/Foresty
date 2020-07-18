527149b0c61f3df379db445a44e004c399b732e7
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2020-07-18 17:41:18 +0700

    Add files via upload

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -10,19 +10,22 @@ def get_ips_for_host(host):
         return ips
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', help='Display URL source code', type=str)
+parser.add_argument('url', type=str)
+parser.add_argument('-u', help='Display URL source code', action='store_true')
 parser.add_argument('-i', help='Display URL IP Adress', action='store_true')
+
 
 args = parser.parse_args()
 
+print(args.url)
 if(args.u):
-    r = requests.get(args.u)
+    r = requests.get(args.url)
     page_source = r.text
-    print("\nURL:", args.u)
+    print("\nURL:", args.url)
     print(page_source)
 if(args.i):
-    url = args.u.replace('http://','')
-    url = args.u.replace('https://','')
+    url = args.url.replace('http://','')
+    url = args.url.replace('https://','')
     ips = get_ips_for_host(url)
     print(repr(ips))
 
